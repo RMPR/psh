a6feef1fef2f9d41bb87fbd2bc14928999f7264e
RMPR <akoudanilo@gmail.com>
2020-12-18 18:58:55 +0100

    Add built-ins

--- src/main.c
+++ src/main.c
@@ -7,12 +7,17 @@
 #include <unistd.h>
 #define PROMPT_STRING_SIZE 100
 #define ARGC 64
+#define NUM_BUILT_INS 3
 
 char* psh_read_config();
 void psh_loop(char* prompt_string);
 char* psh_read_line(void);
 char** psh_split_args(char* line);
 int psh_execute_line(char** args);
+int psh_cd(char* path);
+int psh_exit();
+int psh_help();
+
 
 int main(int argc, char **argv) {
     // run the command loop reading the config file if any
@@ -66,19 +71,19 @@ char* psh_read_line(void) {
 
     chars_read = getline(&line, &len, stdin);
     line[chars_read-1] = ' ';
-    printf("p");
+    printf("\np");
     for(int i=0; i < chars_read; i++) printf("s");
     printf("h");
     return line;
 
 }
 
-char** psh_split_args(char* line){
+char** psh_split_args(char* line) {
     // Split line in args to feed the execution function
     char** args = malloc(ARGC * sizeof(char*));
     int i = 0;
 
-    if(!args) {
+    if (!args) {
         fprintf(stderr, "Allocation failed");
         exit(EXIT_FAILURE);
     }
@@ -86,7 +91,7 @@ char** psh_split_args(char* line){
     for(char* arg = strtok(line, " "); arg != NULL; arg = strtok(NULL, " "), i++) {
         args[i] = strdup(arg);
 
-        if(i == ARGC-1) {
+        if (i == ARGC-1) {
             args = realloc(args, 2 * ARGC * sizeof(char*)); 
         }
     }
@@ -94,16 +99,24 @@ char** psh_split_args(char* line){
     return args;
 }
 
-int psh_execute_line(char** args){
+int psh_execute_line(char** args) {
     // Execute the command
     pid_t pid;
     int status = 0;
+    
+    // Verify beforehand that it's not a built-in
+    if (strcmp(args[0], "cd") == 0) {
+        return psh_cd(args[1]);
+    } else if (strcmp(args[0], "exit") == 0)
+        exit(psh_exit());
+    else if (strcmp(args[0], "help") == 0)
+        return psh_help();
 
-    if ((pid = fork()) < 0){
+    if ((pid = fork()) < 0) {
         fprintf(stderr, "Forking process failed");
         exit(EXIT_FAILURE);
     } else if (pid == 0) {
-        if(execvp(args[0], args) < 0) {
+        if (execvp(args[0], args) < 0) {
             fprintf(stderr, "Execution of the command failed");
             _exit(EXIT_FAILURE);
         }
@@ -111,5 +124,22 @@ int psh_execute_line(char** args){
         waitpid(pid, &status, 0);
     }
 
+    return EXIT_SUCCESS;
+}
+
+int psh_cd(char* path) {
+    int status = 0;
+    if ((status = chdir(path)) != 0) {
+        fprintf(stderr, "Error when changing directories"); 
+    }
+    return status;
+}
+
+int psh_exit(){
+    return EXIT_SUCCESS;
+}
+
+int psh_help() {
+    printf("RMPR psh version whatever");     
     return EXIT_SUCCESS;
 }
